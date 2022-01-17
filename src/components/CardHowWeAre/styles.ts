3b22b092a4c35e5e8aa19e296ae7a50a87b7ff85
SwannyAlves <sksilva@riachuelo.com.br>
2022-01-17 19:57:06 -0300

    style: change margin CardHowWeAreComponent

--- src/components/CardHowWeAre/styles.ts
+++ src/components/CardHowWeAre/styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.div`
   height: 378px;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-  margin: auto;
+  margin: auto 0;
   border-radius: 10px;
   padding: 18px;
   display: flex;
