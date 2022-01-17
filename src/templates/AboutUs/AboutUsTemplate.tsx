a9ec058c913a9fe26768901f24351c62ca9c1905
SwannyAlves <sksilva@riachuelo.com.br>
2022-01-17 19:49:38 -0300

    feat: structure about us

--- src/templates/AboutUs/AboutUsTemplate.tsx
+++ src/templates/AboutUs/AboutUsTemplate.tsx
@@ -1,13 +1,25 @@
 import { Breadcrumb } from 'components/Breadcrumb'
+import { CardHowWeAre } from 'components/CardHowWeAre'
 import { AuthorsProps } from 'types/api'
 import * as S from './styles'
 
 export const AboutUsTemplate = ({ authors }: AuthorsProps) => {
-  console.log(authors)
   return (
     <S.Container>
       <Breadcrumb slug="Quem somos" />
-      {/* TODO - criar página de quem somos */}
+      <S.AboutUs>
+        {authors?.map((author) => (
+          <CardHowWeAre
+            key={author.id}
+            id={author.id}
+            name={author.name}
+            photo={author.photo}
+            role={author.role}
+            description={author.description}
+            socialLinks={author.socialLinks}
+          />
+        ))}
+      </S.AboutUs>
     </S.Container>
   )
 }
--- src/templates/AboutUs/styles.ts
+++ src/templates/AboutUs/styles.ts
@@ -7,6 +7,15 @@ export const Container = styled.div`
         max-width: ${theme.grid.container};
         margin: 6rem auto;
         padding: 0 1rem;
+        width: 100%;
       }
     `}
+`
+export const AboutUs = styled.div`
+  display: flex;
+  margin-top: 4.8rem;
+  gap: 9.6rem;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: left;
 `
