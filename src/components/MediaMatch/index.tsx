5f3127b0ac5c81601a6379044fba24869e12547f
leticiafrontend <leticia.alesilva@outlook.com>
2022-01-19 10:15:19 -0300

    feat: add MediaMatch component

---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,21 @@
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
+import { MediaMatchProps, breakpoint } from './types'
+
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoint) => css`
+    ${media.lessThan(size)` display: block `}
+  `,
+
+  greaterThan: (size: breakpoint) => css`
+    ${media.greaterThan(size)` display: block `}
+  `
+}
+
+export default styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`
