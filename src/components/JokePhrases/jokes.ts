8b4429c2ced86ad49f6878180257f55c72bde8ae
leticiafrontend <leticia.alesilva@outlook.com>
2022-01-24 20:10:22 -0300

    feat: add phrases jokes

--- src/components/JokePhrases/jokes.ts
+++ src/components/JokePhrases/jokes.ts
@@ -1,14 +1,57 @@
-export const jokes = [
+interface IJokes {
+  id: number
+  phrase: string
+  author: string
+}
+
+export const jokes: IJokes = [
   {
     id: 1,
     phrase:
       'Se até as variáveis precisam ser declaradas porque não me declarar pra você?',
     author: 'Hebert Barros'
+  },
+  {
+    id: 2,
+    phrase:
+      'Programadores e artistas são os únicos profissionais que tem como hobby a própria profissão.',
+    author: 'Rafael Lain'
+  },
+  {
+    id: 3,
+    phrase:
+      'Você é o Internet Explore? Porque com você meu layout fica completamente desconfigurado.',
+    author: 'Hebert Barros'
+  },
+  {
+    id: 4,
+    phrase:
+      'Não importa o quão poderoso você se torne, nunca tente fazer tudo sozinho. Caso contrario irá falhar.',
+    author: 'Uchiha Itachi'
+  },
+  {
+    id: 5,
+    phrase: 'No meu computador funciona!',
+    author: 'Todos os programadores do mundo'
+  },
+  {
+    id: 6,
+    phrase: 'Posso te chamar de CSS? Porque eu gosto do seu estilo.',
+    author: 'Desconhecido'
+  },
+  {
+    id: 7,
+    phrase: 'Não trate como constante quem te trata como variável.',
+    author: 'Desconhecido'
+  },
+  {
+    id: 8,
+    phrase: 'Não trate como constante quem te trata como variável.',
+    author: 'Desconhecido'
+  },
+  {
+    id: 9,
+    phrase: 'Minha arte é a programação.',
+    author: 'Deidara Programador'
   }
 ]
-
-// {
-//   "id": 1,
-//   "phrase": "",
-//   "author": ""
-// }
