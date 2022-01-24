c2fec14fc9d60db8b6ff0ba616352b1a277429eb
leticiafrontend <leticia.alesilva@outlook.com>
2022-01-24 10:33:23 -0300

    feat: add query post search

---
+++ src/graphql/queries/getPostsSearch.ts
@@ -0,0 +1,29 @@
+import { gql } from 'graphql-request'
+
+const GET_POST_SLUG = gql`
+  fragment imageData on UploadFile {
+    alternativeText
+    url
+  }
+
+  query GET_POST_SEARCH($text: String!) {
+    postPages(where: { text_contains: $text }) {
+      id
+      cover {
+        ...imageData
+      }
+      created_at
+      title
+      text
+      slug
+      author {
+        name
+        photo {
+          ...imageData
+        }
+      }
+    }
+  }
+`
+
+export default GET_POST_SLUG
