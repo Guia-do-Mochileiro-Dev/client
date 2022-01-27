13561d6274e2ff95515e7255459157feab014d58
leticiafrontend <leticia.alesilva@outlook.com>
2022-01-27 19:46:18 -0300

    style: change font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,31 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+@font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.tff') format('tff');
+  }
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.tff') format('tff');
+  }
+  @font-face {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.tff') format('tff');
+  }
+
   * {
     margin: 0;
     padding: 0;
--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -8,7 +8,7 @@ export default {
   },
   font: {
     family:
-      "'Yu Gothic UI', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      "Roboto, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     light: 300,
     normal: 400,
     bold: 600,
