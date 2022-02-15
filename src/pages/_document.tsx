7a70695a77e8b1914321ce40bbf3bcec495185d9
leticiafrontend <leticia.alesilva@outlook.com>
2022-02-15 09:02:32 -0300

    fix: add tag gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,14 +44,14 @@ export default class MyDocument extends Document {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','G-5RVQ4C9NKZ');`
+})(window,document,'script','dataLayer','GTM-KDF585W'');`
             }}
           ></script>
         </Head>
         <body>
           <noscript
             dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=G-5RVQ4C9NKZ"
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDF585W'"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>`
             }}
           />
